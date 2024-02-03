5046048c9c5a79c0192a4b4e68d58a1688d3cfb5
Valik <valik3201@gmail.com>
2024-02-03 18:21:30 +0100

    refactor: migrate code to React hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,5 @@
-/**
- * React component representing the main application.
- * @extends Component
- */
-import { Component } from 'react';
-
-// Importing the fetchImages function from the services/api module
+import React, { useState, useEffect, useCallback } from 'react';
 import fetchImages from 'services/api';
-
-// Importing various React components for the application
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
 import ImagePortalWelcome from './ImagePortalWelcome';
@@ -16,171 +8,99 @@ import SearchResultInfo from './SearchResultInfo';
 import ErrorAlert from './ErrorAlert';
 import NoResultsAlert from './NoResultsAlert';
 import Loader from './Loader';
-
-// Importing Bootstrap styles and components
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 
 /**
- * Main App component representing the image search application.
- * @class
+ * Main application component for the Image Finder.
+ * @returns {JSX.Element} - The rendered React element.
  */
-class App extends Component {
-  /**
-   * State of the App component.
-   * @type {Object}
-   * @property {Array} images - Array to store fetched images.
-   * @property {string} searchQuery - Current search query.
-   * @property {number} currentPage - Current page number for paginated results.
-   * @property {boolean} isLoading - Flag indicating whether images are being loaded.
-   * @property {string|null} error - Error message if image fetching fails.
-   * @property {number|null} totalHits - Total number of hits for the current search query.
-   */
-  state = {
-    images: [],
-    searchQuery: '',
-    currentPage: 1,
-    isLoading: false,
-    error: null,
-    totalHits: null,
-  };
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [totalHits, setTotalHits] = useState(null);
 
   /**
-   * Fetches images from the Pixabay API based on the current search query and page number.
-   * Updates the component state accordingly.
-   * @async
+   * Fetches images from the API based on the search query and current page.
    * @function
-   * @returns {Promise<void>}
+   * @returns {void}
    */
-  getImages = async () => {
-    const { searchQuery, currentPage } = this.state;
-
-    // If there is no search query, return early
+  const getImages = useCallback(async () => {
     if (!searchQuery) {
       return;
     }
 
     try {
-      // Set loading state to true
-      this.setState({
-        isLoading: true,
-      });
+      setIsLoading(true);
 
-      // Fetch images from Pixabay API
       const { hits, totalHits } = await fetchImages(searchQuery, currentPage);
 
-      // Append new images to the existing ones
-      this.setState(prev => ({
-        images: [...prev.images, ...hits],
-        error: null,
-        totalHits: totalHits || 0,
-      }));
+      setImages(prevImages => [...prevImages, ...hits]);
+      setError(null);
+      setTotalHits(totalHits || 0);
     } catch (error) {
-      // Handle errors by setting the error state
-      this.setState({
-        error: error.message,
-      });
+      setError(error.message);
     } finally {
-      // Set loading state to false regardless of success or failure
-      this.setState({
-        isLoading: false,
-      });
+      setIsLoading(false);
     }
-  };
+  }, [searchQuery, currentPage]);
 
   /**
-   * Lifecycle method that gets called whenever the component updates.
-   * Calls the getImages method if the searchQuery or currentPage changes.
-   * @param {Object} _prevProps - Previous properties.
-   * @param {Object} prevState - Previous state.
-   * @returns {Promise<void>}
-   */
-  async componentDidUpdate(_prevProps, prevState) {
-    if (
-      prevState.searchQuery !== this.state.searchQuery ||
-      prevState.currentPage !== this.state.currentPage
-    ) {
-      this.getImages();
-    }
-  }
-
-  /**
-   * Updates the currentPage in the state, triggering a new set of images to be fetched.
+   * Loads more images by incrementing the current page.
    * @function
    * @returns {void}
    */
-  loadMoreImages = () => {
-    this.setState(prev => ({
-      currentPage: prev.currentPage + 1,
-    }));
+  const loadMoreImages = () => {
+    setCurrentPage(prevPage => prevPage + 1);
   };
 
   /**
-   * Handles form submission, updates the searchQuery, resets images, and sets currentPage to 1.
-   * @param {string} searchQuery - The new search query.
+   * Handles form submission for a new search query.
+   * Resets state and triggers a new image search.
+   * @function
+   * @param {string} newSearchQuery - The new search query.
    * @returns {void}
    */
-  handleSubmit = searchQuery => {
-    if (searchQuery === this.state.searchQuery) {
+  const handleSubmit = newSearchQuery => {
+    if (newSearchQuery === searchQuery) {
       return;
     }
 
-    this.setState({
-      searchQuery: searchQuery,
-      images: [],
-      currentPage: 1,
-    });
+    setSearchQuery(newSearchQuery);
+    setImages([]);
+    setCurrentPage(1);
   };
 
-  /**
-   * Renders the main application component with various child components based on the state.
-   * @function
-   * @returns {JSX.Element} - The rendered React element.
-   */
-  render() {
-    const { images, searchQuery, isLoading, error, totalHits } = this.state;
+  useEffect(() => {
+    getImages();
+  }, [getImages]);
 
-    return (
-      <>
-        {/* Search bar component */}
-        <Searchbar onSubmit={this.handleSubmit} />
+  return (
+    <>
+      <Searchbar onSubmit={handleSubmit} />
 
-        {/* Main container for the application */}
-        <Container className="d-flex flex-column justify-content-center mb-5 mx-auto">
-          {isLoading && <Loader />}{' '}
-          {/* Loader component while images are being fetched */}
-          {error && <ErrorAlert errorMessage={error} />}{' '}
-          {/* Error alert component */}
-          {images.length > 0 && !error && (
-            <>
-              {/* Information about the search results */}
-              <SearchResultInfo
-                searchQuery={searchQuery}
-                totalHits={totalHits}
-              />
+      <Container className="d-flex flex-column justify-content-center mb-5 mx-auto">
+        {isLoading && <Loader />}
+        {error && <ErrorAlert errorMessage={error} />}
+        {images.length > 0 && !error && (
+          <>
+            <SearchResultInfo searchQuery={searchQuery} totalHits={totalHits} />
+            <ImageGallery images={images} />
+            {totalHits > images.length && (
+              <LoadMoreBtn onClick={loadMoreImages} />
+            )}
+          </>
+        )}
+        {!error && searchQuery && totalHits === 0 && (
+          <NoResultsAlert searchQuery={searchQuery} />
+        )}
+        {!searchQuery && <ImagePortalWelcome />}
+      </Container>
+    </>
+  );
+};
 
-              {/* Image gallery component */}
-              <ImageGallery images={images} />
-
-              {/* Load more button if there are more images to load */}
-              {totalHits > images.length && (
-                <LoadMoreBtn onClick={this.loadMoreImages} />
-              )}
-            </>
-          )}
-          {/* No results alert if no images are found */}
-          {!error && searchQuery && totalHits === 0 && (
-            <NoResultsAlert searchQuery={searchQuery} />
-          )}
-          {/* Welcome message for the image portal if no search query is present */}
-          {!searchQuery && <ImagePortalWelcome />}
-        </Container>
-      </>
-    );
-  }
-}
-
-/**
- * Exporting the App component as the default export of the module.
- */
 export default App;
--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,183 +1,110 @@
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Component } from 'react';
 import { Container, Col, Row, Image } from 'react-bootstrap';
 import { ImageModal } from './Modal';
 
 /**
- * React component representing an image gallery.
- * @class
- * @extends Component
+ * Represents an individual item in the ImageGallery.
+ * @param {Object} props - The properties passed to the component.
+ * @param {number} props.id - The unique identifier for the image.
+ * @param {string} props.largeImageURL - The URL of the large version of the image.
+ * @param {string} props.tags - Tags associated with the image.
+ * @param {Function} props.openModal - Function to be called when the image is clicked, opening the modal.
+ * @param {string} props.user - User information associated with the image.
+ * @param {string} props.userImageURL - URL of the user's profile image.
+ * @returns {JSX.Element} - The rendered React element.
  */
-class ImageGallery extends Component {
-  constructor(props) {
-    super(props);
+const ImageGalleryItem = ({
+  id,
+  largeImageURL,
+  tags,
+  openModal,
+  user,
+  userImageURL,
+}) => (
+  <Col
+    key={id}
+    onClick={() => openModal({ id, largeImageURL, tags, user, userImageURL })}
+  >
+    <Image
+      src={largeImageURL}
+      alt={tags}
+      role="button"
+      rounded
+      style={{ objectFit: 'cover', height: '100%', width: '100%' }}
+    />
+  </Col>
+);
 
-    this.state = {
-      showModal: false,
-      selectedImage: null,
-    };
-  }
-
-  /**
-   * Opens the modal with the selected image.
-   * @function
-   * @param {Object} image - The selected image to be displayed in the modal.
-   * @returns {void}
-   */
-  openModal = image => {
-    this.setState({
-      showModal: true,
-      selectedImage: image,
-    });
-  };
-
-  /**
-   * Closes the modal.
-   * @function
-   * @returns {void}
-   */
-  closeModal = () => {
-    this.setState({
-      showModal: false,
-      selectedImage: null,
-    });
-  };
-
-  /**
-   * Renders the ImageGallery component.
-   * @function
-   * @returns {JSX.Element} - The rendered React element.
-   */
-  render() {
-    const { images } = this.props;
-    const { showModal, selectedImage } = this.state;
-
-    return (
-      <Row>
-        {/* Container for the image gallery */}
-        <Container className="justify-content-center">
-          <Row xs={1} sm={2} md={3} lg={4} className="g-4">
-            {/* Map through the images array and render ImageGalleryItem for each image */}
-            {images.map(
-              ({
-                id,
-                webformatURL,
-                largeImageURL,
-                tags,
-                user,
-                userImageURL,
-              }) => (
-                <ImageGallery.Item
-                  key={id}
-                  id={id}
-                  webformatURL={webformatURL}
-                  largeImageURL={largeImageURL}
-                  tags={tags}
-                  user={user}
-                  userImageURL={userImageURL}
-                  openModal={this.openModal}
-                />
-              )
-            )}
-          </Row>
-
-          {/* ImageModal component for displaying a larger image in a modal */}
-          <ImageModal
-            showModal={showModal}
-            selectedImage={selectedImage}
-            closeModal={this.closeModal}
-          />
-        </Container>
-      </Row>
-    );
-  }
-}
-
-/**
- * PropTypes for the ImageGallery component.
- * @static
- * @type {Object}
- */
-ImageGallery.propTypes = {
-  /**
-   * Array of images to be displayed in the gallery.
-   */
-  images: PropTypes.array.isRequired,
-};
-
-/**
- * React component representing an item in the ImageGallery.
- * @class
- * @extends Component
- */
-class ImageGalleryItem extends Component {
-  /**
-   * Renders the ImageGalleryItem component.
-   * @function
-   * @returns {JSX.Element} - The rendered React element.
-   */
-  render() {
-    const { id, largeImageURL, tags, openModal, user, userImageURL } =
-      this.props;
-
-    return (
-      <Col
-        key={id}
-        onClick={() =>
-          openModal({ id, largeImageURL, tags, user, userImageURL })
-        }
-      >
-        {/* Image component displaying the image with a rounded border */}
-        <Image
-          src={largeImageURL}
-          alt={tags}
-          role="button"
-          rounded
-          style={{ objectFit: 'cover', height: '100%', width: '100%' }}
-        />
-      </Col>
-    );
-  }
-}
-
-/**
- * PropTypes for the ImageGalleryItem component.
- * @static
- * @type {Object}
- */
 ImageGalleryItem.propTypes = {
-  /**
-   * Unique identifier for the image.
-   */
   id: PropTypes.number.isRequired,
-  /**
-   * URL of the large version of the image.
-   */
   largeImageURL: PropTypes.string.isRequired,
-  /**
-   * Tags associated with the image.
-   */
   tags: PropTypes.string.isRequired,
-  /**
-   * Function to be called when the image is clicked, opening the modal.
-   */
   openModal: PropTypes.func.isRequired,
-  /**
-   * User information associated with the image.
-   */
   user: PropTypes.string.isRequired,
-  /**
-   * URL of the user's profile image.
-   */
   userImageURL: PropTypes.string.isRequired,
 };
 
 /**
- * Assigns the ImageGalleryItem component as a property of the ImageGallery component.
+ * Represents a gallery of images.
+ * @param {Object} props - The properties passed to the component.
+ * @param {Array} props.images - Array of images to be displayed in the gallery.
+ * @returns {JSX.Element} - The rendered React element.
  */
-ImageGallery.Item = ImageGalleryItem;
+const ImageGallery = ({ images }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
 
-/**
- * Exporting the ImageGallery component as the default export of the module.
- */
+  /**
+   * Opens the modal with the selected image.
+   * @param {Object} image - The selected image.
+   * @returns {void}
+   */
+  const openModal = image => {
+    setShowModal(true);
+    setSelectedImage(image);
+  };
+
+  /**
+   * Closes the modal.
+   * @returns {void}
+   */
+  const closeModal = () => {
+    setShowModal(false);
+    setSelectedImage(null);
+  };
+
+  return (
+    <Row>
+      <Container className="justify-content-center">
+        <Row xs={1} sm={2} md={3} lg={4} className="g-4">
+          {images.map(
+            ({ id, webformatURL, largeImageURL, tags, user, userImageURL }) => (
+              <ImageGalleryItem
+                key={id}
+                id={id}
+                webformatURL={webformatURL}
+                largeImageURL={largeImageURL}
+                tags={tags}
+                user={user}
+                userImageURL={userImageURL}
+                openModal={openModal}
+              />
+            )
+          )}
+        </Row>
+        <ImageModal
+          showModal={showModal}
+          selectedImage={selectedImage}
+          closeModal={closeModal}
+        />
+      </Container>
+    </Row>
+  );
+};
+
+ImageGallery.propTypes = {
+  images: PropTypes.array.isRequired,
+};
+
 export default ImageGallery;
